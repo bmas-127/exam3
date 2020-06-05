47e5e6dbbc2e878c186547f55be88d77fc54fc36
bmas-127 <h881109@gmail.com>
2020-06-05 19:45:34 +0800

    first commit

---
+++ recieve_and_publish.py
@@ -0,0 +1,166 @@
+import serial
+
+import time
+
+import paho.mqtt.client as paho
+
+
+mqttc = paho.Client()
+
+
+# Settings for connection
+
+host = "10.0.2.15"
+
+topic= "Mbed"
+
+port = 1883
+
+
+# Callbacks
+
+def on_connect(self, mosq, obj, rc):
+
+    print("Connected rc: " + str(rc))
+
+
+def on_message(mosq, obj, msg):
+
+    print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n");
+
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+
+    print("Subscribed OK")
+
+
+def on_unsubscribe(mosq, obj, mid, granted_qos):
+
+    print("Unsubscribed OK")
+
+
+# Set callbacks
+
+mqttc.on_message = on_message
+
+mqttc.on_connect = on_connect
+
+mqttc.on_subscribe = on_subscribe
+
+mqttc.on_unsubscribe = on_unsubscribe
+
+
+# Connect and subscribe
+
+print("Connecting to " + host + "/" + topic)
+
+mqttc.connect(host, port=1883, keepalive=60)
+
+mqttc.subscribe(topic, 0)
+
+
+# XBee setting
+
+serdev = '/dev/ttyUSB0'
+
+s = serial.Serial(serdev, 9600)
+
+
+s.write("+++".encode())
+
+char = s.read(2)
+
+print("Enter AT mode.")
+
+print(char.decode())
+
+
+s.write("ATMY 0x109\r\n".encode())
+
+char = s.read(3)
+
+print("Set MY 0x109.")
+
+print(char.decode())
+
+
+s.write("ATDL 0x209\r\n".encode())
+
+char = s.read(3)
+
+print("Set DL 0x209.")
+
+print(char.decode())
+
+
+s.write("ATID 0x1\r\n".encode())
+
+char = s.read(3)
+
+print("Set PAN ID 0x1.")
+
+print(char.decode())
+
+
+s.write("ATWR\r\n".encode())
+
+char = s.read(3)
+
+print("Write config.")
+
+print(char.decode())
+
+
+s.write("ATMY\r\n".encode())
+
+char = s.read(4)
+
+print("MY :")
+
+print(char.decode())
+
+
+s.write("ATDL\r\n".encode())
+
+char = s.read(4)
+
+print("DL : ")
+
+print(char.decode())
+
+
+s.write("ATCN\r\n".encode())
+
+char = s.read(3)
+
+print("Exit AT mode.")
+
+print(char.decode())
+
+
+print("start sending RPC")
+
+
+serdev = '/dev/ttyACM0'
+dick = serial.Serial(serdev)
+
+while True:
+
+    # send RPC to remote
+
+    dick.write("/getAcc/run\r".encode())
+
+    time.sleep(1)
+
+    # recieve and send to remote
+    for i in range (0,3) :
+        line=dick.readline()
+
+        mqttc.publish(topic, line)
+
+        print(line)
+
+        time.sleep(1)
+
+
+s.close()
---
+++ suscribe.py
@@ -0,0 +1,110 @@
+import paho.mqtt.client as paho
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import serial
+
+t = np.arange(0, 100)
+delta = np.zeros((100))
+
+
+
+
+
+
+
+# https://os.mbed.com/teams/mqtt/wiki/Using-MQTT#python-client
+
+
+# MQTT broker hosted on local machine
+
+mqttc = paho.Client()
+
+
+# Settings for connection
+
+# TODO: revise host to your ip
+
+host = "10.0.2.15"
+
+topic = "Mbed"
+
+i = 0
+
+
+# Callbacks
+
+def draw(num) :
+      delta[i] = num
+      i = i + 1
+      print(i)
+      print("dick")
+      print("dick")
+
+def on_connect(self, mosq, obj, rc):
+
+      print("Connected rc: " + str(rc))
+
+
+def on_message(mosq, obj, msg):
+      print(float(msg.payload))
+      
+#      elif i == 100 :
+#            fig, ax = plt.subplots(2, 1)
+#            ax[0].plot(t,delta)
+#            ax[1].plot(t,delta) 
+#            plt.show()
+
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+
+      print("Subscribed OK")
+
+
+def on_unsubscribe(mosq, obj, mid, granted_qos):
+
+      print("Unsubscribed OK")
+
+
+# Set callbacks
+
+mqttc.on_message = on_message
+
+mqttc.on_connect = on_connect
+
+mqttc.on_subscribe = on_subscribe
+
+mqttc.on_unsubscribe = on_unsubscribe
+
+
+# Connect and subscribe
+
+print("Connecting to " + host + "/" + topic)
+
+mqttc.connect(host, port=1883, keepalive=60)
+
+mqttc.subscribe(topic, 0)
+
+
+# Publish messages from Python
+
+num = 0
+
+while num != 5:
+
+      ret = mqttc.publish(topic, "Message from Python!\n", qos=0)
+
+      if (ret[0] != 0):
+
+            print("Publish failed")
+
+      mqttc.loop()
+
+      time.sleep(1.5)
+
+      num += 1
+
+
+# Loop forever, receiving messages
+
+mqttc.loop_forever()
